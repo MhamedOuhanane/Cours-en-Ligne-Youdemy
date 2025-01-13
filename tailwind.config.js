a4ad37394555ff5754b9dc5275600c52eacafe76
MhamedOuhanane <mhmdeouhnan60@gmail.com>
2025-01-13 13:15:48 +0100

    Créer la structure de dossiers du projet et l'instalation de tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{php,html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        karla: ['Karla', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
